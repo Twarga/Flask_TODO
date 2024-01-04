e2e9bc2a15947fb4511822f0b7024875c402e2ca
Youness Touzani <103898544+Twarga@users.noreply.github.com>
2024-01-04 13:31:08 +0100

    all buged fixed jump to next phase

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ class Todo(db.Model):
 @app.route("/")
 def home():
     todo_list = Todo.query.all()
+    return render_template("base.html" , todo_list=todo_list)
 
 
 @app.route("/add" , methods=["POST"])
